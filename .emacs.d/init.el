b6296c1d8cbc9bd5c085ae8a7b0f1b1e766250d2
Tianchi Wu <tianchiwu.27@gmail.com>
2025-01-09 15:40:28 -0500

    Update emacs configurations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,21 @@
+(add-to-list 'load-path "~/.emacs.d/external")
+
 (set-language-environment "utf-8")
 (set-default-coding-systems 'utf-8-unix)
 (set-keyboard-coding-system 'utf-8-unix)
 (set-terminal-coding-system 'utf-8-unix)
+
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+	     '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdown\\|mdwn\\)\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+  "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md'" . gfm-mode))
+
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "C-c C-e") #'markdown-do))
 
 (setq inhibit-startup-screen t)
 
@@ -23,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cousine Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 130 :width normal)))))
+ '(default ((t (:family "M+CodeLat60 Nerd Font" :foundry "UFDN" :slant normal :weight regular :height 128 :width expanded)))))
 
 (add-hook 'python-mode-hook
 	  (lambda ()
@@ -33,3 +47,4 @@
 	  (lambda ()
 	    (setq-default java-indent-offset 2)))
 
+(message "init.el loaded successfully!")
